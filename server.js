19022df14c7bcb82961db12142967f9d457c1b7f
Fernando Rafael Narbona <fernandorafaelnarbona@MacBook-Pro.local>
2019-05-01 02:56:32 -0400

    add quotes page and redirect to forms

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.post('/sendMail', async (req, res) => {
     subject: `General Inquiry: ${req.body.userName}`,
     text:
     `General Inquiry...
-    Phone: ${req.body.userPhone}
+    Phone: ${req.body.userPhone} \n
     Message: ${req.body.userMessage}
     `
   };
@@ -43,24 +43,24 @@ app.post('/boatApp', async (req, res) => {
     from: req.body.userEmail,
     subject: `Marine Insurance: ${req.body.userName}`,
     text:
-    `Marine Insurance...
-	  Phone: ${req.userPhone}
-    Date of Birth: ${req.userDOB}
-    Address: ${req.userAddress}
-    Vessel Name: ${req.vesselName}
-    Vessel Make: ${req.vesselMake}
-    Vessel Length: ${req.vesselLength}
-    Vessel Year: ${req.vesselYear}
-    Vessel Engine: ${req.vesselEngine}
-    Vessel HP: ${req.vesselHP}
-    Operating Captain: ${req.opCaptain}
-    Operator's Experience: ${req.opExp}
-    Vessel Prior/Current Insurance: ${req.vesselPriorIns}
-    Vessel Prior/Current Insurance Expiration: ${req.vesselPriorExp}
-    Previously driven vessels:
-    ${req.vessel1}
-    ${req.vessel2}
-    ${req.vessel3}
+    `Marine Insurance... \n
+	  Phone: ${req.body.userPhone} \n
+    Date of Birth: ${req.body.userDOB} \n
+    Address: ${req.body.userAddress} \n
+    Vessel Name: ${req.body.vesselName} \n
+    Vessel Make: ${req.body.vesselMake} \n
+    Vessel Length: ${req.body.vesselLength} \n
+    Vessel Year: ${req.body.vesselYear} \n
+    Vessel Engine: ${req.body.vesselEngine} \n
+    Vessel HP: ${req.body.vesselHP} \n
+    Operating Captain: ${req.body.opCaptain} \n
+    Operator's Experience: ${req.body.opExp} \n
+    Vessel Prior/Current Insurance: ${req.body.vesselPriorIns} \n
+    Vessel Prior/Current Insurance Expiration: ${req.body.vesselPriorExp} \n
+    Previously driven vessels: \n
+    ${req.body.vessel1} \n
+    ${req.body.vessel2} \n
+    ${req.body.vessel3} \n
     `
   };
 
@@ -85,21 +85,21 @@ app.post('/houseApp', async (req, res) => {
     subject: `Home Insurance: ${req.body.userName}`,
     text:
     `Home Insurance... \n
-    Phone: ${req.userPhone} \n
-    Date of Birth: ${req.userDOB} \n
-    Address: ${req.userAddress} \n
-    Property Address: ${req.propAddress} \n
-    Year Built: ${req.propYear} \n
-    Size of Property (sq ft): ${req.propSize} \n
-    Construction Type: ${req.propConsType} \n
-    Roof Type: ${req.propRoofType} \n
-    Floors: ${req.propFloors} \n
-    Beds: ${req.propBed} \n
-    Bath: ${req.propBath} \n
-    Market Value: ${preq.ropValue} \n
-    Insurance: ${req.insName} \n
-    Insurance Expiration Date: ${req.insExpire} \n
-    Losses?: ${req.insLoss} \n
+    Phone: ${req.body.userPhone} \n
+    Date of Birth: ${req.body.userDOB} \n
+    Address: ${req.body.userAddress} \n
+    Property Address: ${req.body.propAddress} \n
+    Year Built: ${req.body.propYear} \n
+    Size of Property (sq ft): ${req.body.propSize} \n
+    Construction Type: ${req.body.propConsType} \n
+    Roof Type: ${req.body.propRoofType} \n
+    Floors: ${req.body.propFloors} \n
+    Beds: ${req.body.propBed} \n
+    Bath: ${req.body.propBath} \n
+    Market Value: ${preq.body.ropValue} \n
+    Insurance: ${req.body.insName} \n
+    Insurance Expiration Date: ${req.body.insExpire} \n
+    Losses?: ${req.body.insLoss} \n
     `
   };
 
@@ -124,21 +124,21 @@ app.post('/carApp', async (req, res) => {
     subject: `Auto Insurance: ${req.body.userName1}`,
     text:
     `Auto Insurance... \n
-    Second Insured ${req.userName2} \n
-    Phone: ${req.userPhone} \n
-    Date of Birth: ${req.userDOB1} \n
-    2nd Insured Date of Birth: ${req.userDOB2} \n
-    Address: ${req.userAddress} \n
-    Driving Experience: ${req.userExp} \n
-    Married?: ${req.userMarried} \n
-    Prior/Current Insurance: ${req.userPriorIns} \n
-    Prior/Current Insurance Expiration: ${req.userPriorExp} \n
-    Primary Driver License: ${req.userLicense1} \n
-    Secondary Driver License: ${req.userLicense2} \n
-    VIN1: ${req.VIN1} \n
-    VIN2: ${req.VIN2} \n
-    Vehicle1: ${req.Vehicle1} \n
-    Vehicle2: ${req.Vehicle2} \n
+    Second Insured ${req.body.userName2} \n
+    Phone: ${req.body.userPhone} \n
+    Date of Birth: ${req.body.userDOB1} \n
+    2nd Insured Date of Birth: ${req.body.userDOB2} \n
+    Address: ${req.body.userAddress} \n
+    Driving Experience: ${req.body.userExp} \n
+    Married?: ${req.body.userMarried} \n
+    Prior/Current Insurance: ${req.body.userPriorIns} \n
+    Prior/Current Insurance Expiration: ${req.body.userPriorExp} \n
+    Primary Driver License: ${req.body.userLicense1} \n
+    Secondary Driver License: ${req.body.userLicense2} \n
+    VIN1: ${req.body.VIN1} \n
+    VIN2: ${req.body.VIN2} \n
+    Vehicle1: ${req.body.Vehicle1} \n
+    Vehicle2: ${req.body.Vehicle2} \n
     `
   };
 
@@ -163,12 +163,12 @@ app.post('/busApp', async (req, res) => {
     subject: `Business Insurance: ${req.body.userName} on behalf of ${req.body.COmpanyName}`,
     text:
     `Business Insurance... \n
-    Company Name: ${req.companyName} \n
-    Company Address: ${req.companyAddress} \n
-    Size of Company (sq ft): ${req.companySize} \n
-    Year Established: ${req.companyYear} \n
-    Number of Employees: ${req.companyEmployees} \n
-    Annual Sales Revenue: ${req.companySales} \n
+    Company Name: ${req.body.companyName} \n
+    Company Address: ${req.body.companyAddress} \n
+    Size of Company (sq ft): ${req.body.companySize} \n
+    Year Established: ${req.body.companyYear} \n
+    Number of Employees: ${req.body.companyEmployees} \n
+    Annual Sales Revenue: ${req.body.companySales} \n
     Prior/Current Insurance: ${companyPriorIns} \n
     Prior/Current Insurance Expiration: ${companyPriorExp} \n
     Business Description: ${companyOperation}
